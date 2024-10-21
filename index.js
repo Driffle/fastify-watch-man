9a552579188dedc88fed76bbf596381fd554a484
gaurav8298 <gaurav@driffle.com>
2024-10-21 17:47:38 +0530

    Removed default import

--- index.js
+++ index.js
@@ -323,5 +323,5 @@ module.exports = fp(fastifyRateLimit, {
   fastify: '4.x',
   name: '@driffle/fastify-watchman'
 })
-module.exports.default = fastifyRateLimit
-module.exports.fastifyRateLimit = fastifyRateLimit
+
+module.exports.RateLimitter = fastifyRateLimit
